4a08ed3965b72a665445c48eb3bf9ac4d9a13812
johnericward <johnericward@gmail.com>
2019-09-23 16:24:12 -0400

    fixed some layout stuff

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,4 +1,4 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+// Make sure to wait to attach handlers until the DOM is fully loaded.
 $(function() {
   $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
