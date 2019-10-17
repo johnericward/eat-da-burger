3df4d369abee0e2cc0abaca6d68c6baca181590f
johnericward <johnericward@gmail.com>
2019-10-16 21:45:10 -0400

    fixed connection var

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
